cbf5e891c34cfe8e87fab733dab1a010796df006
Shubham-Lal <im.shubhamlal@gmail.com>
2024-05-08 23:35:41 +0530

    modify footer

--- src/pages/home/components/Footer/index.jsx
+++ src/pages/home/components/Footer/index.jsx
@@ -5,10 +5,12 @@ const Footer = () => {
     return (
         <footer id='footer'>
             <div className='left'>© Talup 2023. All rights reserved</div>
+            <p className='made-by-desktop'>Made with ❤️ by <a href='https://github.com/Shubham-Lal' target='_blank' rel='noopener noreferrer' style={{ textDecoration: 'underline' }}>Shubham Lal</a></p>
             <div className='right'>
                 <Link to='/'>Terms & Conditions</Link>
                 <Link to='/'>Privacy Policy</Link>
             </div>
+            <p className='made-by-mobile'>Made with ❤️ by <a href='https://github.com/Shubham-Lal' target='_blank' rel='noopener noreferrer' style={{ textDecoration: 'underline' }}>Shubham Lal</a></p>
         </footer>
     )
 }
